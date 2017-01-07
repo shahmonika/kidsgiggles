482c5832c37df56860d0de85401037523fc9d896
shahmonika <shaah.ms@gmail.com>
2017-01-07 12:05:02 -0500

    submit joke component

--- app/components/submit-joke/submit-joke.component.ts
+++ app/components/submit-joke/submit-joke.component.ts
@@ -1,30 +1,23 @@
-
-import { Component ,OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import '../../rxjs-operators';
 import {Joke} from '../../joke';
 import {JokeService} from '../../joke.service';
-
-
-
-
-
 @Component({
-
-    moduleId: module.id,                     //creating metadata{
-selector: 'submit-joke',
+    moduleId: module.id,
+    selector: 'submit-joke',
     templateUrl: 'submit-joke.component.html'
-
 })
-export class SubmitJokeComponent{
-    joke:Joke[];
-    jokeses:Joke[];
-    constructor(private jokeService:JokeService){}
+export class SubmitJokeComponent {
+    joke: Joke[];
+    jokeses: Joke[];
 
-    addJoke (description:string){
-    if(!description){return;}
+    constructor(private jokeService: JokeService) {
+    }
+
+    addJoke(description: string) {
+        if (!description) {
+            return;
+        }
         this.jokeService.addJoke(description).subscribe(joke=>this.jokeses.push(joke));
     }
-    // @Input()
-    // joke: Joke;
-
 }
